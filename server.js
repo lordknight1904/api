aeab1d432dae8e0ce816d1a6d7f7838f5ae0941c
Khoa <lordknight@Khoas-MacBook-Pro.local>
2018-05-18 10:15:15 +0700

    18/5 1

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import config from './config';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 // ROUTES FOR OUR API
