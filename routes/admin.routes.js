63a0cc6cda223245669206146c69f2058db0fdc5
Khoa <lordknight@Khoas-MacBook-Pro.local>
2018-05-31 09:32:01 +0700

    31/5

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,10 +4,10 @@ import handler from './handler';
 const router = new Router();
 
 router.route('/admin/authenticate').post(Controller.authenticate, handler);
-router.route('/admin').post(Controller.addAdmin, handler);
 router.route('/admin').get(Controller.getAllAdmin, handler);
+router.route('/admin').post(Controller.addAdmin, handler);
+router.route('/admin').put(Controller.updateAdmin, handler);
+router.route('/admin').delete(Controller.deleteAdmin, handler);
 router.route('/admin/:id').get(Controller.getAdmin, handler);
-router.route('/admin/:id').put(Controller.updateAdmin, handler);
-router.route('/admin/:id').delete(Controller.deleteAdmin, handler);
 
 export default router;
